6dff3f324f8b349482d8e7db99a2690d4f317a03
Lohithsst <tanuku_sai_surya_lohith@s2020.ssts.edu.sg>
2023-07-10 11:04:18 +0800

    minor change

--- script.js
+++
@@ -1,8 +0,0 @@
-// This is a simple JavaScript function to test if your setup is working
-function testSetup() {
-    console.log("Congratulations! Your setup is working correctly.");
-  }
-  
-  // Call the testSetup function
-  testSetup();
-  
